de88305c29ff74a2168281eed188173cf2d94ef1
Tim Jensen <timothy.r.jensen@boeing.com>
2019-02-28 22:02:39 -0500

    commit

--- SSDT/src/Northwind.data.sql/dbo/Tables/Territories.sql
+++ SSDT/src/Northwind.data.sql/dbo/Tables/Territories.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Territories] (
     [TerritoryID]          NVARCHAR (20) NOT NULL,
-    [TerritoryDescription] NCHAR (60)    NOT NULL,
+    [TerritoryDescription] NCHAR (70)    NOT NULL,
     [RegionID]             INT           NOT NULL,
     CONSTRAINT [PK_Territories] PRIMARY KEY NONCLUSTERED ([TerritoryID] ASC),
     CONSTRAINT [FK_Territories_Region] FOREIGN KEY ([RegionID]) REFERENCES [dbo].[Region] ([RegionID])
